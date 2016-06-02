58f04e99f8a02016caf955a9638ff0b0ba84ffa1
Frank S. Thomas <frank@timepit.eu>
2016-06-02 20:55:26 +0200

    More coverage

--- src/test/scala/singleton/ops/PlusSpec.scala
+++ src/test/scala/singleton/ops/PlusSpec.scala
@@ -33,4 +33,11 @@ class PlusSpec extends Properties("Plus") {
     val p3 = Plus[p1.Out, p2.Out]
     sameType[p3.Out, W.`10`.T]
   }
+
+  property("W(1) + W(2) == 3") = secure {
+    val w1 = W(1)
+    val w2 = W(2)
+    val p1 = Plus[w1.T, w2.T]
+    sameType[p1.Out, W.`3`.T]
+  }
 }
