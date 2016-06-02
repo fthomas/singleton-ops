abe5d6d8149aded4ca84875a15f1956a6eb80c49
Frank S. Thomas <frank@timepit.eu>
2016-06-02 07:44:06 +0200

    Update tests

--- src/test/scala/singleton/ops/TestUtils.scala
+++ src/test/scala/singleton/ops/TestUtils.scala
@@ -1,5 +1,12 @@
 package singleton.ops
 
+import org.scalacheck.Prop
+
 object TestUtils {
   def sameType[A, B](implicit ev: A =:= B): Boolean = true
+
+  def wellTyped(body: => Unit): Prop = Prop.secure {
+    body
+    true
+  }
 }
--- src/test/scala/singleton/ops/TimesSpec.scala
+++ src/test/scala/singleton/ops/TimesSpec.scala
@@ -2,16 +2,21 @@ package singleton.ops
 
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
+import shapeless.test.illTyped
 import singleton.ops.TestUtils._
 
 class TimesSpec extends Properties("Times") {
   property("2 * 3 == 6") = secure {
-    val times = Times[W.`2`.T, W.`3`.T]
-    sameType[times.Out, W.`6`.T]
+    val t1 = Times[W.`2`.T, W.`3`.T]
+    sameType[t1.Out, W.`6`.T]
   }
 
   property("1.5 * 2.0 == 3.0") = secure {
-    val times = Times[W.`1.5`.T, W.`2.0`.T]
-    sameType[times.Out, W.`3.0`.T]
+    val t1 = Times[W.`1.5`.T, W.`2.0`.T]
+    sameType[t1.Out, W.`3.0`.T]
+  }
+
+  property("Int * 0 = ???") = wellTyped {
+    illTyped(""" Times[Int, W.`0`.T] """)
   }
 }
