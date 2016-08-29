c6fe2ff9e20a3bda618d7206e1b4f0df9709c706
Oron Port <soronpo@campus.technion.ac.il>
2016-08-29 20:42:01 +0300

    temporary

--- src/main/scala/singleton/ops/Plus.scala
+++ src/main/scala/singleton/ops/Plus.scala
@@ -4,7 +4,13 @@ import macrocompat.bundle
 import scala.reflect.macros.whitebox
 import singleton.ops.impl._
 
-trait Plus[A, B] extends Op
+trait Plus[A, B] extends Op {
+  //override type Out <: Int
+}
+
+trait PlusI[A, B] extends Plus[A, B] {
+  override type Out <: Int
+}
 
 object Plus extends Op2Companion[Plus] {
 
@@ -18,5 +24,20 @@ object Plus extends Op2Companion[Plus] {
         nt: c.Expr[Numeric[T]]
     ): c.Tree =
       materializeOp2[Plus, A, B].usingFunction(evalTyped(nt).plus)
+  }
+}
+
+object PlusI extends Op2Companion[PlusI] {
+
+  implicit def materializePlusI[T, A <: T, B <: T](
+                                                   implicit nt: Numeric[T]
+                                                 ): PlusI[A, B] = macro PlusIMacro.materialize[T, A, B]
+
+  @bundle
+  final class PlusIMacro(val c: whitebox.Context) extends Macros {
+    def materialize[T, A: c.WeakTypeTag, B: c.WeakTypeTag](
+                                                            nt: c.Expr[Numeric[T]]
+                                                          ): c.Tree =
+      materializeOp2[PlusI, A, B].usingFunction(evalTyped(nt).plus)
   }
 }
