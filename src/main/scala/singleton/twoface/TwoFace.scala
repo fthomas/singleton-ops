682690c58de04ea197aa9ad8851739a0c499cb1c
Oron Port <soronpo@campus.technion.ac.il>
2017-04-27 22:18:11 +0300

    TwoFace

--- src/main/scala/singleton/twoface/TwoFace.scala
+++ src/main/scala/singleton/twoface/TwoFace.scala
@@ -10,6 +10,7 @@ object TwoFace {
   }
 
   sealed trait TwoFaceBuilder[TF[_], Face] {
+    type Op[OP] = TwoFaceOp[TF, Face, OP]
     def apply[T](value : Face) : TF[T]
     implicit def safe[T <: Face with Singleton](value : T)(implicit tfb : TwoFaceBuilder[TF, Face]) : TF[T] =
       tfb[T](value)
@@ -31,33 +32,31 @@ object TwoFace {
   }
 
   final class Char[T] private(val value : scala.Char) extends AnyVal with TwoFaceAny[scala.Char, T] {
-    def + [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
-    def - [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
-    def * [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
-    def / [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
-    def + [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
-    def - [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
-    def * [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
-    def / [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
+    def + [R](r : Char[R])(implicit tfo : Int.Op[T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Char[R])(implicit tfo : Int.Op[T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Char[R])(implicit tfo : Int.Op[T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Char[R])(implicit tfo : Int.Op[T / R]) = tfo(this.getValue / r.getValue)
+    def + [R](r : Int[R])(implicit tfo : Int.Op[T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Int[R])(implicit tfo : Int.Op[T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Int[R])(implicit tfo : Int.Op[T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Int[R])(implicit tfo : Int.Op[T / R]) = tfo(this.getValue / r.getValue)
     @inline def getValue : scala.Char = value
   }
-
   implicit object Char extends TwoFaceBuilder[Char, scala.Char] {
     def apply[T](value : scala.Char) = new Char[T](value)
   }
 
   final class Int[T] private[twoface](val value : scala.Int) extends AnyVal with TwoFaceAny[scala.Int, T] {
-    def + [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
-    def - [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
-    def * [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
-    def / [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
-    def + [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
-    def - [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
-    def * [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
-    def / [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
+    def + [R](r : Char[R])(implicit tfo : Int.Op[T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Char[R])(implicit tfo : Int.Op[T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Char[R])(implicit tfo : Int.Op[T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Char[R])(implicit tfo : Int.Op[T / R]) = tfo(this.getValue / r.getValue)
+    def + [R](r : Int[R])(implicit tfo : Int.Op[T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Int[R])(implicit tfo : Int.Op[T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Int[R])(implicit tfo : Int.Op[T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Int[R])(implicit tfo : Int.Op[T / R]) = tfo(this.getValue / r.getValue)
     @inline def getValue : scala.Int = value
   }
-
   implicit object Int extends TwoFaceBuilder[Int, scala.Int] {
     def apply[T](value : scala.Int) = new Int[T](value)
   }
