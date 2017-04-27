7cee98930d9e1c9968fe16ca2f714d3d6dc46810
Oron Port <soronpo@campus.technion.ac.il>
2017-04-27 19:34:06 +0300

    TwoFace

--- src/main/scala/singleton/twoface/TwoFace.scala
+++ src/main/scala/singleton/twoface/TwoFace.scala
@@ -9,47 +9,57 @@ object TwoFace {
     override def toString = getValue.toString
   }
 
-  sealed trait TwoFaceObj[TF[_], Face] {
-    protected[twoface] def create[T](value : Face) : TF[T]
-    protected[twoface] def op[T](op : => Face)(implicit fb : FallBack[Face, T]) : TF[fb.Out] =
-      create[fb.Out](if (fb.isLiteral) fb.value.get else op)
-    def apply[T](value : Face)(implicit fb : FallBack[Face, T]) : TF[fb.Out] =
-      create[fb.Out](value)
-    implicit def safe[T <: Face with Singleton](value : T) : TF[T] = create[T](value)
-    implicit def unsafe[T <: Face](value : T) : TF[Face] = create[Face](value)
-    implicit def ev[T](implicit si : SafeCast[Face with Singleton, T]) : TF[si.Out] = create[si.Out](si.value)
+  sealed trait TwoFaceBuilder[TF[_], Face] {
+    def apply[T](value : Face) : TF[T]
+    implicit def safe[T <: Face with Singleton](value : T)(implicit tfb : TwoFaceBuilder[TF, Face]) : TF[T] =
+      tfb[T](value)
+    implicit def unsafe[T <: Face](value : T)(implicit tfb : TwoFaceBuilder[TF, Face]) : TF[Face] =
+      tfb[Face](value)
+    implicit def ev[T](implicit si : SafeCast[Face with Singleton, T], tfb : TwoFaceBuilder[TF, Face]) : TF[si.Out] =
+      tfb[si.Out](si.value)
+  }
+
+  sealed trait TwoFaceOp[TF[_], Face, OP] {
+    type FB <: FallBack[Face, OP]
+    val fb : FB
+    def apply(op : => Face)(implicit tfb : TwoFaceBuilder[TF, Face]) : TF[fb.Out] =
+      tfb[fb.Out](if (fb.isLiteral) fb.value.get else op)
+  }
+  object TwoFaceOp {
+    implicit def ev[TF[_], Face, OP](implicit fb0 : FallBack[Face, OP]) : TwoFaceOp[TF, Face, OP]{type FB = fb0.type} =
+      new TwoFaceOp[TF, Face, OP]{type FB = fb0.type; val fb : FB = fb0}
   }
 
   final class Char[T] private(val value : scala.Char) extends AnyVal with TwoFaceAny[scala.Char, T] {
-    def + [R](r : Char[R])(implicit fb : FallBack[scala.Char, T + R]) = Int.op[T + R](this.getValue + r.getValue)
-    def - [R](r : Char[R])(implicit fb : FallBack[scala.Char, T - R]) = Int.op[T - R](this.getValue - r.getValue)
-    def * [R](r : Char[R])(implicit fb : FallBack[scala.Char, T * R]) = Int.op[T * R](this.getValue * r.getValue)
-    def / [R](r : Char[R])(implicit fb : FallBack[scala.Char, T / R]) = Int.op[T / R](this.getValue / r.getValue)
-    def + [R](r : Int[R])(implicit fb : FallBack[scala.Int, T + R]) = Int.op[T + R](this.getValue + r.getValue)
-    def - [R](r : Int[R])(implicit fb : FallBack[scala.Int, T - R]) = Int.op[T - R](this.getValue - r.getValue)
-    def * [R](r : Int[R])(implicit fb : FallBack[scala.Int, T * R]) = Int.op[T * R](this.getValue * r.getValue)
-    def / [R](r : Int[R])(implicit fb : FallBack[scala.Int, T / R]) = Int.op[T / R](this.getValue / r.getValue)
+    def + [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
+    def + [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
     @inline def getValue : scala.Char = value
   }
 
-  object Char extends TwoFaceObj[Char, scala.Char] {
-    protected[twoface] def create[T](value : scala.Char) = new Char[T](value)
+  implicit object Char extends TwoFaceBuilder[Char, scala.Char] {
+    def apply[T](value : scala.Char) = new Char[T](value)
   }
 
-  final class Int[T] private(val value : scala.Int) extends AnyVal with TwoFaceAny[scala.Int, T] {
-    def + [R](r : Char[R])(implicit fb : FallBack[scala.Char, T + R]) = Int.op[T + R](this.getValue + r.getValue)
-    def - [R](r : Char[R])(implicit fb : FallBack[scala.Char, T - R]) = Int.op[T - R](this.getValue - r.getValue)
-    def * [R](r : Char[R])(implicit fb : FallBack[scala.Char, T * R]) = Int.op[T * R](this.getValue * r.getValue)
-    def / [R](r : Char[R])(implicit fb : FallBack[scala.Char, T / R]) = Int.op[T / R](this.getValue / r.getValue)
-    def + [R](r : Int[R])(implicit fb : FallBack[scala.Int, T + R]) = Int.op[T + R](this.getValue + r.getValue)
-    def - [R](r : Int[R])(implicit fb : FallBack[scala.Int, T - R]) = Int.op[T - R](this.getValue - r.getValue)
-    def * [R](r : Int[R])(implicit fb : FallBack[scala.Int, T * R]) = Int.op[T * R](this.getValue * r.getValue)
-    def / [R](r : Int[R])(implicit fb : FallBack[scala.Int, T / R]) = Int.op[T / R](this.getValue / r.getValue)
+  final class Int[T] private[twoface](val value : scala.Int) extends AnyVal with TwoFaceAny[scala.Int, T] {
+    def + [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Char[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
+    def + [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T + R]) = tfo(this.getValue + r.getValue)
+    def - [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T - R]) = tfo(this.getValue - r.getValue)
+    def * [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T * R]) = tfo(this.getValue * r.getValue)
+    def / [R](r : Int[R])(implicit tfo : TwoFaceOp[Int, scala.Int, T / R]) = tfo(this.getValue / r.getValue)
     @inline def getValue : scala.Int = value
   }
 
-  object Int extends TwoFaceObj[Int, scala.Int] {
-    protected[twoface] def create[T](value : scala.Int) = new Int[T](value)
+  implicit object Int extends TwoFaceBuilder[Int, scala.Int] {
+    def apply[T](value : scala.Int) = new Int[T](value)
   }
 
 }
--- src/main/scala/singleton/twoface/package.scala
+++ src/main/scala/singleton/twoface/package.scala
@@ -3,6 +3,6 @@ package singleton
 package object twoface {
   @inline implicit def fromTwoFaceUnsafe[Face, T <: Face](tf : TwoFace.TwoFaceAny[Face, T]) : Face = tf.getValue
   @inline implicit def fromTwoFaceSafe[Face, T <: Face with Singleton](tf : TwoFace.TwoFaceAny[Face, T])
-    (implicit sc: ValueOf[T]) : T = valueOf[T]
+    (implicit sc: ValueOf[T]) : T {} = valueOf[T]
 
 }
