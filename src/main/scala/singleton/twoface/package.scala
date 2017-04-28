3ebd15ddd1e9d0d1229185bb74c5e4a627f09a2e
Oron Port <soronpo@campus.technion.ac.il>
2017-04-28 05:02:25 +0300

    TwoFace

--- src/main/scala/singleton/twoface/package.scala
+++ src/main/scala/singleton/twoface/package.scala
@@ -1,7 +1,7 @@
 package singleton
 
 package object twoface {
-  @inline implicit def fromTwoFaceUnsafe[Face, T <: Face](tf : TwoFace.TwoFaceAny[Face, T]) : Face = tf.getValue
+  @inline implicit def fromTwoFaceUnsafe[Face, T](tf : TwoFace.TwoFaceAny[Face, T]) : Face = tf.getValue
   @inline implicit def fromTwoFaceSafe[Face, T <: Face with Singleton](tf : TwoFace.TwoFaceAny[Face, T])
     (implicit sc: ValueOf[T]) : T {} = valueOf[T]
 
