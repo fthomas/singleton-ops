6522bda60ebf739f13107efe2d68be5072e64159
Oron Port <soronpo@campus.technion.ac.il>
2017-06-11 15:24:02 +0300

    Added <, >, <=, >= tests

--- src/test/scala/singleton/ops/BTSpec.scala
+++ src/test/scala/singleton/ops/BTSpec.scala
@@ -27,7 +27,7 @@ class BTSpec extends Properties(">") {
   type resultFalse = false
   type resultTrue = true
 
-  def verifySTNum[L,R](implicit
+  def verifyBTNum[L,R](implicit
                        verifyFalse: Verify[Negate[L] > R, resultFalse],
                        verifyFalse2: Verify[L > L, resultFalse],
                        verifyTrue: Verify[L > R, resultTrue]) : Prop = wellTyped {}
@@ -35,12 +35,12 @@ class BTSpec extends Properties(">") {
   ////////////////////////////////////////////////////////////////////////
   // Nat op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Nat, Nat arguments") = verifySTNum[leftNat,rightNat]
-  property("Nat, Char arguments") = verifySTNum[leftNat,rightChar]
-  property("Nat, Int arguments") = verifySTNum[leftNat,rightInt]
-  property("Nat, Long arguments") = verifySTNum[leftNat,rightLong]
-  property("Nat, Float arguments") = verifySTNum[leftNat,rightFloat]
-  property("Nat, Double arguments") = verifySTNum[leftNat,rightDouble]
+  property("Nat, Nat arguments") = verifyBTNum[leftNat,rightNat]
+  property("Nat, Char arguments") = verifyBTNum[leftNat,rightChar]
+  property("Nat, Int arguments") = verifyBTNum[leftNat,rightInt]
+  property("Nat, Long arguments") = verifyBTNum[leftNat,rightLong]
+  property("Nat, Float arguments") = verifyBTNum[leftNat,rightFloat]
+  property("Nat, Double arguments") = verifyBTNum[leftNat,rightDouble]
   property("Nat, String arguments") = {illTyped("""implicitly[OP[leftNat,rightString]]"""); true}
   property("Nat, Boolean arguments") = {illTyped("""implicitly[OP[leftNat,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -48,12 +48,12 @@ class BTSpec extends Properties(">") {
   ////////////////////////////////////////////////////////////////////////
   // Char op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Char, Nat arguments") = verifySTNum[leftChar,rightNat]
-  property("Char, Char arguments") = verifySTNum[leftChar,rightChar]
-  property("Char, Int arguments") = verifySTNum[leftChar,rightInt]
-  property("Char, Long arguments") = verifySTNum[leftChar,rightLong]
-  property("Char, Float arguments") = verifySTNum[leftChar,rightFloat]
-  property("Char, Double arguments") = verifySTNum[leftChar,rightDouble]
+  property("Char, Nat arguments") = verifyBTNum[leftChar,rightNat]
+  property("Char, Char arguments") = verifyBTNum[leftChar,rightChar]
+  property("Char, Int arguments") = verifyBTNum[leftChar,rightInt]
+  property("Char, Long arguments") = verifyBTNum[leftChar,rightLong]
+  property("Char, Float arguments") = verifyBTNum[leftChar,rightFloat]
+  property("Char, Double arguments") = verifyBTNum[leftChar,rightDouble]
   property("Char, String arguments") = {illTyped("""implicitly[OP[leftChar,rightString]]"""); true}
   property("Char, Boolean arguments") = {illTyped("""implicitly[OP[leftChar,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -61,12 +61,12 @@ class BTSpec extends Properties(">") {
   ////////////////////////////////////////////////////////////////////////
   // Int op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Int, Nat arguments") = verifySTNum[leftInt,rightNat]
-  property("Int, Char arguments") = verifySTNum[leftInt,rightChar]
-  property("Int, Int arguments") = verifySTNum[leftInt,rightInt]
-  property("Int, Long arguments") = verifySTNum[leftInt,rightLong]
-  property("Int, Float arguments") = verifySTNum[leftInt,rightFloat]
-  property("Int, Double arguments") = verifySTNum[leftInt,rightDouble]
+  property("Int, Nat arguments") = verifyBTNum[leftInt,rightNat]
+  property("Int, Char arguments") = verifyBTNum[leftInt,rightChar]
+  property("Int, Int arguments") = verifyBTNum[leftInt,rightInt]
+  property("Int, Long arguments") = verifyBTNum[leftInt,rightLong]
+  property("Int, Float arguments") = verifyBTNum[leftInt,rightFloat]
+  property("Int, Double arguments") = verifyBTNum[leftInt,rightDouble]
   property("Int, String arguments") = {illTyped("""implicitly[OP[leftInt,rightString]]"""); true}
   property("Int, Boolean arguments") = {illTyped("""implicitly[OP[leftInt,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -74,12 +74,12 @@ class BTSpec extends Properties(">") {
   ////////////////////////////////////////////////////////////////////////
   // Long op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Long, Nat arguments") = verifySTNum[leftLong,rightNat]
-  property("Long, Char arguments") = verifySTNum[leftLong,rightChar]
-  property("Long, Int arguments") = verifySTNum[leftLong,rightInt]
-  property("Long, Long arguments") = verifySTNum[leftLong,rightLong]
-  property("Long, Float arguments") = verifySTNum[leftLong,rightFloat]
-  property("Long, Double arguments") = verifySTNum[leftLong,rightDouble]
+  property("Long, Nat arguments") = verifyBTNum[leftLong,rightNat]
+  property("Long, Char arguments") = verifyBTNum[leftLong,rightChar]
+  property("Long, Int arguments") = verifyBTNum[leftLong,rightInt]
+  property("Long, Long arguments") = verifyBTNum[leftLong,rightLong]
+  property("Long, Float arguments") = verifyBTNum[leftLong,rightFloat]
+  property("Long, Double arguments") = verifyBTNum[leftLong,rightDouble]
   property("Long, String arguments") = {illTyped("""implicitly[OP[leftLong,rightString]]"""); true}
   property("Long, Boolean arguments") = {illTyped("""implicitly[OP[leftLong,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -87,12 +87,12 @@ class BTSpec extends Properties(">") {
   ////////////////////////////////////////////////////////////////////////
   // Float op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Float, Nat arguments") = verifySTNum[leftFloat,rightNat]
-  property("Float, Char arguments") = verifySTNum[leftFloat,rightChar]
-  property("Float, Int arguments") = verifySTNum[leftFloat,rightInt]
-  property("Float, Long arguments") = verifySTNum[leftFloat,rightLong]
-  property("Float, Float arguments") = verifySTNum[leftFloat,rightFloat]
-  property("Float, Double arguments") = verifySTNum[leftFloat,rightDouble]
+  property("Float, Nat arguments") = verifyBTNum[leftFloat,rightNat]
+  property("Float, Char arguments") = verifyBTNum[leftFloat,rightChar]
+  property("Float, Int arguments") = verifyBTNum[leftFloat,rightInt]
+  property("Float, Long arguments") = verifyBTNum[leftFloat,rightLong]
+  property("Float, Float arguments") = verifyBTNum[leftFloat,rightFloat]
+  property("Float, Double arguments") = verifyBTNum[leftFloat,rightDouble]
   property("Float, String arguments") = {illTyped("""implicitly[OP[leftFloat,rightString]]"""); true}
   property("Float, Boolean arguments") = {illTyped("""implicitly[OP[leftFloat,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -100,12 +100,12 @@ class BTSpec extends Properties(">") {
   ////////////////////////////////////////////////////////////////////////
   // Double op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Double, Nat arguments") = verifySTNum[leftDouble,rightNat]
-  property("Double, Char arguments") = verifySTNum[leftDouble,rightChar]
-  property("Double, Int arguments") = verifySTNum[leftDouble,rightInt]
-  property("Double, Long arguments") = verifySTNum[leftDouble,rightLong]
-  property("Double, Float arguments") = verifySTNum[leftDouble,rightFloat]
-  property("Double, Double arguments") = verifySTNum[leftDouble,rightDouble]
+  property("Double, Nat arguments") = verifyBTNum[leftDouble,rightNat]
+  property("Double, Char arguments") = verifyBTNum[leftDouble,rightChar]
+  property("Double, Int arguments") = verifyBTNum[leftDouble,rightInt]
+  property("Double, Long arguments") = verifyBTNum[leftDouble,rightLong]
+  property("Double, Float arguments") = verifyBTNum[leftDouble,rightFloat]
+  property("Double, Double arguments") = verifyBTNum[leftDouble,rightDouble]
   property("Double, String arguments") = {illTyped("""implicitly[OP[leftDouble,rightString]]"""); true}
   property("Double, Boolean arguments") = {illTyped("""implicitly[OP[leftDouble,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
--- src/test/scala/singleton/ops/STESpec.scala
+++ src/test/scala/singleton/ops/STESpec.scala
@@ -27,20 +27,20 @@ class STESpec extends Properties("<=") {
   type resultFalse = false
   type resultTrue = true
 
-  def verifySTNum[L,R](implicit
-                       verifyFalse: Verify[L <= Negate[R], resultFalse],
-                       verifyTrue: Verify[L <= L, resultTrue],
-                       verifyTrue2: Verify[L <= R, resultTrue]) : Prop = wellTyped {}
+  def verifySTENum[L,R](implicit
+                        verifyFalse: Verify[L <= Negate[R], resultFalse],
+                        verifyTrue: Verify[L <= L, resultTrue],
+                        verifyTrue2: Verify[L <= R, resultTrue]) : Prop = wellTyped {}
 
   ////////////////////////////////////////////////////////////////////////
   // Nat op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Nat, Nat arguments") = verifySTNum[leftNat,rightNat]
-  property("Nat, Char arguments") = verifySTNum[leftNat,rightChar]
-  property("Nat, Int arguments") = verifySTNum[leftNat,rightInt]
-  property("Nat, Long arguments") = verifySTNum[leftNat,rightLong]
-  property("Nat, Float arguments") = verifySTNum[leftNat,rightFloat]
-  property("Nat, Double arguments") = verifySTNum[leftNat,rightDouble]
+  property("Nat, Nat arguments") = verifySTENum[leftNat,rightNat]
+  property("Nat, Char arguments") = verifySTENum[leftNat,rightChar]
+  property("Nat, Int arguments") = verifySTENum[leftNat,rightInt]
+  property("Nat, Long arguments") = verifySTENum[leftNat,rightLong]
+  property("Nat, Float arguments") = verifySTENum[leftNat,rightFloat]
+  property("Nat, Double arguments") = verifySTENum[leftNat,rightDouble]
   property("Nat, String arguments") = {illTyped("""implicitly[OP[leftNat,rightString]]"""); true}
   property("Nat, Boolean arguments") = {illTyped("""implicitly[OP[leftNat,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -48,12 +48,12 @@ class STESpec extends Properties("<=") {
   ////////////////////////////////////////////////////////////////////////
   // Char op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Char, Nat arguments") = verifySTNum[leftChar,rightNat]
-  property("Char, Char arguments") = verifySTNum[leftChar,rightChar]
-  property("Char, Int arguments") = verifySTNum[leftChar,rightInt]
-  property("Char, Long arguments") = verifySTNum[leftChar,rightLong]
-  property("Char, Float arguments") = verifySTNum[leftChar,rightFloat]
-  property("Char, Double arguments") = verifySTNum[leftChar,rightDouble]
+  property("Char, Nat arguments") = verifySTENum[leftChar,rightNat]
+  property("Char, Char arguments") = verifySTENum[leftChar,rightChar]
+  property("Char, Int arguments") = verifySTENum[leftChar,rightInt]
+  property("Char, Long arguments") = verifySTENum[leftChar,rightLong]
+  property("Char, Float arguments") = verifySTENum[leftChar,rightFloat]
+  property("Char, Double arguments") = verifySTENum[leftChar,rightDouble]
   property("Char, String arguments") = {illTyped("""implicitly[OP[leftChar,rightString]]"""); true}
   property("Char, Boolean arguments") = {illTyped("""implicitly[OP[leftChar,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -61,12 +61,12 @@ class STESpec extends Properties("<=") {
   ////////////////////////////////////////////////////////////////////////
   // Int op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Int, Nat arguments") = verifySTNum[leftInt,rightNat]
-  property("Int, Char arguments") = verifySTNum[leftInt,rightChar]
-  property("Int, Int arguments") = verifySTNum[leftInt,rightInt]
-  property("Int, Long arguments") = verifySTNum[leftInt,rightLong]
-  property("Int, Float arguments") = verifySTNum[leftInt,rightFloat]
-  property("Int, Double arguments") = verifySTNum[leftInt,rightDouble]
+  property("Int, Nat arguments") = verifySTENum[leftInt,rightNat]
+  property("Int, Char arguments") = verifySTENum[leftInt,rightChar]
+  property("Int, Int arguments") = verifySTENum[leftInt,rightInt]
+  property("Int, Long arguments") = verifySTENum[leftInt,rightLong]
+  property("Int, Float arguments") = verifySTENum[leftInt,rightFloat]
+  property("Int, Double arguments") = verifySTENum[leftInt,rightDouble]
   property("Int, String arguments") = {illTyped("""implicitly[OP[leftInt,rightString]]"""); true}
   property("Int, Boolean arguments") = {illTyped("""implicitly[OP[leftInt,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -74,12 +74,12 @@ class STESpec extends Properties("<=") {
   ////////////////////////////////////////////////////////////////////////
   // Long op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Long, Nat arguments") = verifySTNum[leftLong,rightNat]
-  property("Long, Char arguments") = verifySTNum[leftLong,rightChar]
-  property("Long, Int arguments") = verifySTNum[leftLong,rightInt]
-  property("Long, Long arguments") = verifySTNum[leftLong,rightLong]
-  property("Long, Float arguments") = verifySTNum[leftLong,rightFloat]
-  property("Long, Double arguments") = verifySTNum[leftLong,rightDouble]
+  property("Long, Nat arguments") = verifySTENum[leftLong,rightNat]
+  property("Long, Char arguments") = verifySTENum[leftLong,rightChar]
+  property("Long, Int arguments") = verifySTENum[leftLong,rightInt]
+  property("Long, Long arguments") = verifySTENum[leftLong,rightLong]
+  property("Long, Float arguments") = verifySTENum[leftLong,rightFloat]
+  property("Long, Double arguments") = verifySTENum[leftLong,rightDouble]
   property("Long, String arguments") = {illTyped("""implicitly[OP[leftLong,rightString]]"""); true}
   property("Long, Boolean arguments") = {illTyped("""implicitly[OP[leftLong,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -87,12 +87,12 @@ class STESpec extends Properties("<=") {
   ////////////////////////////////////////////////////////////////////////
   // Float op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Float, Nat arguments") = verifySTNum[leftFloat,rightNat]
-  property("Float, Char arguments") = verifySTNum[leftFloat,rightChar]
-  property("Float, Int arguments") = verifySTNum[leftFloat,rightInt]
-  property("Float, Long arguments") = verifySTNum[leftFloat,rightLong]
-  property("Float, Float arguments") = verifySTNum[leftFloat,rightFloat]
-  property("Float, Double arguments") = verifySTNum[leftFloat,rightDouble]
+  property("Float, Nat arguments") = verifySTENum[leftFloat,rightNat]
+  property("Float, Char arguments") = verifySTENum[leftFloat,rightChar]
+  property("Float, Int arguments") = verifySTENum[leftFloat,rightInt]
+  property("Float, Long arguments") = verifySTENum[leftFloat,rightLong]
+  property("Float, Float arguments") = verifySTENum[leftFloat,rightFloat]
+  property("Float, Double arguments") = verifySTENum[leftFloat,rightDouble]
   property("Float, String arguments") = {illTyped("""implicitly[OP[leftFloat,rightString]]"""); true}
   property("Float, Boolean arguments") = {illTyped("""implicitly[OP[leftFloat,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
@@ -100,12 +100,12 @@ class STESpec extends Properties("<=") {
   ////////////////////////////////////////////////////////////////////////
   // Double op XXX
   ////////////////////////////////////////////////////////////////////////
-  property("Double, Nat arguments") = verifySTNum[leftDouble,rightNat]
-  property("Double, Char arguments") = verifySTNum[leftDouble,rightChar]
-  property("Double, Int arguments") = verifySTNum[leftDouble,rightInt]
-  property("Double, Long arguments") = verifySTNum[leftDouble,rightLong]
-  property("Double, Float arguments") = verifySTNum[leftDouble,rightFloat]
-  property("Double, Double arguments") = verifySTNum[leftDouble,rightDouble]
+  property("Double, Nat arguments") = verifySTENum[leftDouble,rightNat]
+  property("Double, Char arguments") = verifySTENum[leftDouble,rightChar]
+  property("Double, Int arguments") = verifySTENum[leftDouble,rightInt]
+  property("Double, Long arguments") = verifySTENum[leftDouble,rightLong]
+  property("Double, Float arguments") = verifySTENum[leftDouble,rightFloat]
+  property("Double, Double arguments") = verifySTENum[leftDouble,rightDouble]
   property("Double, String arguments") = {illTyped("""implicitly[OP[leftDouble,rightString]]"""); true}
   property("Double, Boolean arguments") = {illTyped("""implicitly[OP[leftDouble,rightBoolean]]"""); true}
   ////////////////////////////////////////////////////////////////////////
