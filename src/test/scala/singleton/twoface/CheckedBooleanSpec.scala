c585d5343fd17b51c733106701aa7ab276290964
Oron Port <soronpo@campus.technion.ac.il>
2017-07-31 21:10:51 +0300

    Checked0Param fully working

--- src/test/scala/singleton/twoface/CheckedBooleanSpec.scala
+++ src/test/scala/singleton/twoface/CheckedBooleanSpec.scala
@@ -1,51 +1,46 @@
-//package singleton.twoface
-//
-//import org.scalacheck.Properties
-//import shapeless.test.illTyped
-//import singleton.TestUtils._
-//import singleton.ops._
-//
-//object CheckedBooleanSpec {
-//  type Cond[T] = T
-//  type Msg[T] = "Failed Check"
-//  @checked0Param[Cond, Msg, Boolean] class CheckedTrue[T]
-//  illTyped("""@checked0Param[Cond, Msg, Boolean] trait CheckedTrueBad[T]""")
-//
-//  implicit object RuntimeChecked extends CheckedTrue.Runtime {
-//    def cond(l : Boolean) : scala.Boolean = l
-//    def msg(l : Boolean) : java.lang.String = s"Failed Check"
-//  }
-//}
-//
-//class CheckedBooleanSpec extends Properties("Checked.Boolean") {
-//  import CheckedBooleanSpec._
-//
-//  def condTrue[T](t : CheckedTrue[T]) : Unit = {t.unsafeCheck()}
-//
-//  property("Compile-time checks") = wellTyped {
-//    condTrue(true)
-//    condTrue(TwoFace.Boolean(true))
-//    illTyped("""condTrue(false)""")
-//    illTyped("""condTrue(TwoFace.Boolean(false))""")
-//  }
-//
-//  property("Run-time checks") = wellTyped {
-//    condTrue(us(true))
-//    condTrue(TwoFace.Boolean(us(true)))
-//    illRun{condTrue(us(false))}
-//    illRun{condTrue(TwoFace.Boolean(us(false)))}
-//  }
-//
-//  def condTrueImpl[T](realValue : Boolean)(implicit t : CheckedTrue.Shell[T]) : Unit = {t(realValue).unsafeCheck()}
-//
-//  property("Shell compile-time checks") = wellTyped {
-//    condTrueImpl[true](true)
-//    illTyped("""condTrueImpl[false](true)""", "Failed Check")
-//    illTyped("""condTrueImpl[false](false)""", "Failed Check")
-//  }
-//
-//  property("Shell run-time checks") = wellTyped {
-//    condTrueImpl[Boolean](true)
-//    illRun{condTrueImpl[Boolean](false)}
-//  }
-//}
+package singleton.twoface
+
+import org.scalacheck.Properties
+import shapeless.test.illTyped
+import singleton.TestUtils._
+import singleton.ops._
+
+object CheckedBooleanSpec {
+  type Cond[T] = T
+  type Msg[T] = "Failed Check"
+  @checked0Param[Cond, Msg, Boolean] class CheckedTrue[T]
+  illTyped("""@checked0Param[Cond, Msg, Boolean] trait CheckedTrueBad[T]""")
+}
+
+class CheckedBooleanSpec extends Properties("Checked.Boolean") {
+  import CheckedBooleanSpec._
+
+  def condTrue[T](t : CheckedTrue[T]) : Unit = {t.unsafeCheck()}
+
+  property("Compile-time checks") = wellTyped {
+    condTrue(true)
+    condTrue(TwoFace.Boolean(true))
+    illTyped("""condTrue(false)""")
+    illTyped("""condTrue(TwoFace.Boolean(false))""")
+  }
+
+  property("Run-time checks") = wellTyped {
+    condTrue(us(true))
+    condTrue(TwoFace.Boolean(us(true)))
+    illRun{condTrue(us(false))}
+    illRun{condTrue(TwoFace.Boolean(us(false)))}
+  }
+
+  def condTrueImpl[T](realValue : Boolean)(implicit t : CheckedTrue.Shell[T]) : Unit = {t(realValue).unsafeCheck()}
+
+  property("Shell compile-time checks") = wellTyped {
+    condTrueImpl[true](true)
+    illTyped("""condTrueImpl[false](true)""", "Failed Check")
+    illTyped("""condTrueImpl[false](false)""", "Failed Check")
+  }
+
+  property("Shell run-time checks") = wellTyped {
+    condTrueImpl[Boolean](true)
+    illRun{condTrueImpl[Boolean](false)}
+  }
+}
--- src/test/scala/singleton/twoface/CheckedDoubleSpec.scala
+++ src/test/scala/singleton/twoface/CheckedDoubleSpec.scala
@@ -1,37 +1,32 @@
-//package singleton.twoface
-//
-//import org.scalacheck.Properties
-//import shapeless.test.illTyped
-//import singleton.TestUtils._
-//import singleton.ops._
-//
-//object CheckedDoubleSpec {
-//  type Cond[T] = T < 50.0
-//  type Msg[T] = "Failed Check"
-//  @checked0Param[Cond, Msg, Double] class CheckedSmallerThan50[T]
-//
-//  implicit object RuntimeChecked extends CheckedSmallerThan50.Runtime {
-//    def cond(l : Double) : scala.Boolean = l < 50.0
-//    def msg(l : Double) : java.lang.String = s"Failed Check"
-//  }
-//}
-//
-//class CheckedDoubleSpec extends Properties("Checked.Double") {
-//  import CheckedDoubleSpec._
-//
-//  def smallerThan50[T](t : CheckedSmallerThan50[T]) : Unit = {t.unsafeCheck()}
-//
-//  property("Compile-time checks") = wellTyped {
-//    smallerThan50(40.0)
-//    smallerThan50(TwoFace.Double(40.0))
-//    illTyped("""smallerThan50(50.0)""")
-//    illTyped("""smallerThan50(TwoFace.Double(50.0))""")
-//  }
-//
-//  property("Run-time checks") = wellTyped {
-//    smallerThan50(us(40.0))
-//    smallerThan50(TwoFace.Double(us(40.0)))
-//    illRun{smallerThan50(us(50.0))}
-//    illRun{smallerThan50(TwoFace.Double(us(50.0)))}
-//  }
-//}
+package singleton.twoface
+
+import org.scalacheck.Properties
+import shapeless.test.illTyped
+import singleton.TestUtils._
+import singleton.ops._
+
+object CheckedDoubleSpec {
+  type Cond[T] = T < 50.0
+  type Msg[T] = "Failed Check"
+  @checked0Param[Cond, Msg, Double] class CheckedSmallerThan50[T]
+}
+
+class CheckedDoubleSpec extends Properties("Checked.Double") {
+  import CheckedDoubleSpec._
+
+  def smallerThan50[T](t : CheckedSmallerThan50[T]) : Unit = {t.unsafeCheck()}
+
+  property("Compile-time checks") = wellTyped {
+    smallerThan50(40.0)
+    smallerThan50(TwoFace.Double(40.0))
+    illTyped("""smallerThan50(50.0)""")
+    illTyped("""smallerThan50(TwoFace.Double(50.0))""")
+  }
+
+  property("Run-time checks") = wellTyped {
+    smallerThan50(us(40.0))
+    smallerThan50(TwoFace.Double(us(40.0)))
+    illRun{smallerThan50(us(50.0))}
+    illRun{smallerThan50(TwoFace.Double(us(50.0)))}
+  }
+}
