ba33341cea3d9968130dc21ac8434365a2973c47
Oron Port <soronpo@campus.technion.ac.il>
2017-08-02 10:21:29 +0300

    slowly converting tests

--- src/test/scala/singleton/ops/MinusSpec.scala
+++ src/test/scala/singleton/ops/MinusSpec.scala
@@ -1,138 +1,138 @@
-//package singleton.ops
-//
-//import org.scalacheck.Properties
-//import shapeless.test.illTyped
-//import singleton.TestUtils._
-//
-//class MinusSpec extends Properties("-") {
-//  type OP[L,R] = -[L,R]
-//  type leftNat = shapeless.Nat._3
-//  type leftChar = '\u0003'
-//  type leftInt = 3
-//  type leftLong = 3L
-//  type leftFloat = 3.0f
-//  type leftDouble = 3.0
-//  type leftString = "Something"
-//  type leftBoolean = true
-//
-//  type rightNat = shapeless.Nat._2
-//  type rightChar = '\u0002'
-//  type rightInt = 2
-//  type rightLong = 2L
-//  type rightFloat = 2.0f
-//  type rightDouble = 2.0
-//  type rightString = "Else"
-//  type rightBoolean = false
-//
-//  type resultChar = '\u0001'
-//  type resultInt = 1
-//  type resultLong = 1L
-//  type resultFloat = 1.0f
-//  type resultDouble = 1.0
-//  type resultString = "SomethingElse"
-//  type resultBoolean = false
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // Nat op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("Nat, Nat arguments") = verifyOp2Args[OP,leftNat,rightNat,resultInt]
-//  property("Nat, Char arguments") = verifyOp2Args[OP,leftNat,rightChar,resultInt]
-//  property("Nat, Int arguments") = verifyOp2Args[OP,leftNat,rightInt,resultInt]
-//  property("Nat, Long arguments") = verifyOp2Args[OP,leftNat,rightLong,resultLong]
-//  property("Nat, Float arguments") = verifyOp2Args[OP,leftNat,rightFloat,resultFloat]
-//  property("Nat, Double arguments") = verifyOp2Args[OP,leftNat,rightDouble,resultDouble]
-//  property("Nat, String arguments") = {illTyped("""implicitly[OP[leftNat,rightString]]"""); true}
-//  property("Nat, Boolean arguments") = {illTyped("""implicitly[OP[leftNat,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // Char op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("Char, Nat arguments") = verifyOp2Args[OP,leftChar,rightNat,resultInt]
-//  property("Char, Char arguments") = verifyOp2Args[OP,leftChar,rightChar,resultInt]
-//  property("Char, Int arguments") = verifyOp2Args[OP,leftChar,rightInt,resultInt]
-//  property("Char, Long arguments") = verifyOp2Args[OP,leftChar,rightLong,resultLong]
-//  property("Char, Float arguments") = verifyOp2Args[OP,leftChar,rightFloat,resultFloat]
-//  property("Char, Double arguments") = verifyOp2Args[OP,leftChar,rightDouble,resultDouble]
-//  property("Char, String arguments") = {illTyped("""implicitly[OP[leftChar,rightString]]"""); true}
-//  property("Char, Boolean arguments") = {illTyped("""implicitly[OP[leftChar,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // Int op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("Int, Nat arguments") = verifyOp2Args[OP,leftInt,rightNat,resultInt]
-//  property("Int, Char arguments") = verifyOp2Args[OP,leftInt,rightChar,resultInt]
-//  property("Int, Int arguments") = verifyOp2Args[OP,leftInt,rightInt,resultInt]
-//  property("Int, Long arguments") = verifyOp2Args[OP,leftInt,rightLong,resultLong]
-//  property("Int, Float arguments") = verifyOp2Args[OP,leftInt,rightFloat,resultFloat]
-//  property("Int, Double arguments") = verifyOp2Args[OP,leftInt,rightDouble,resultDouble]
-//  property("Int, String arguments") = {illTyped("""implicitly[OP[leftInt,rightString]]"""); true}
-//  property("Int, Boolean arguments") = {illTyped("""implicitly[OP[leftInt,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // Long op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("Long, Nat arguments") = verifyOp2Args[OP,leftLong,rightNat,resultLong]
-//  property("Long, Char arguments") = verifyOp2Args[OP,leftLong,rightChar,resultLong]
-//  property("Long, Int arguments") = verifyOp2Args[OP,leftLong,rightInt,resultLong]
-//  property("Long, Long arguments") = verifyOp2Args[OP,leftLong,rightLong,resultLong]
-//  property("Long, Float arguments") = verifyOp2Args[OP,leftLong,rightFloat,resultFloat]
-//  property("Long, Double arguments") = verifyOp2Args[OP,leftLong,rightDouble,resultDouble]
-//  property("Long, String arguments") = {illTyped("""implicitly[OP[leftLong,rightString]]"""); true}
-//  property("Long, Boolean arguments") = {illTyped("""implicitly[OP[leftLong,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // Float op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("Float, Nat arguments") = verifyOp2Args[OP,leftFloat,rightNat,resultFloat]
-//  property("Float, Char arguments") = verifyOp2Args[OP,leftFloat,rightChar,resultFloat]
-//  property("Float, Int arguments") = verifyOp2Args[OP,leftFloat,rightInt,resultFloat]
-//  property("Float, Long arguments") = verifyOp2Args[OP,leftFloat,rightLong,resultFloat]
-//  property("Float, Float arguments") = verifyOp2Args[OP,leftFloat,rightFloat,resultFloat]
-//  property("Float, Double arguments") = verifyOp2Args[OP,leftFloat,rightDouble,resultDouble]
-//  property("Float, String arguments") = {illTyped("""implicitly[OP[leftFloat,rightString]]"""); true}
-//  property("Float, Boolean arguments") = {illTyped("""implicitly[OP[leftFloat,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // Double op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("Double, Nat arguments") = verifyOp2Args[OP,leftDouble,rightNat,resultDouble]
-//  property("Double, Char arguments") = verifyOp2Args[OP,leftDouble,rightChar,resultDouble]
-//  property("Double, Int arguments") = verifyOp2Args[OP,leftDouble,rightInt,resultDouble]
-//  property("Double, Long arguments") = verifyOp2Args[OP,leftDouble,rightLong,resultDouble]
-//  property("Double, Float arguments") = verifyOp2Args[OP,leftDouble,rightFloat,resultDouble]
-//  property("Double, Double arguments") = verifyOp2Args[OP,leftDouble,rightDouble,resultDouble]
-//  property("Double, String arguments") = {illTyped("""implicitly[OP[leftDouble,rightString]]"""); true}
-//  property("Double, Boolean arguments") = {illTyped("""implicitly[OP[leftDouble,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // String op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("String, Nat arguments") = {illTyped("""implicitly[OP[leftString,rightNat]]"""); true}
-//  property("String, Char arguments") = {illTyped("""implicitly[OP[leftString,rightChar]]"""); true}
-//  property("String, Int arguments") = {illTyped("""implicitly[OP[leftString,rightInt]]"""); true}
-//  property("String, Long arguments") = {illTyped("""implicitly[OP[leftString,rightLong]]"""); true}
-//  property("String, Float arguments") = {illTyped("""implicitly[OP[leftString,rightFloat]]"""); true}
-//  property("String, Double arguments") = {illTyped("""implicitly[OP[leftString,rightDouble]]"""); true}
-//  property("String, String arguments") = {illTyped("""implicitly[OP[leftString,rightString]]"""); true}
-//  property("String, Boolean arguments") = {illTyped("""implicitly[OP[leftString,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//
-//  ////////////////////////////////////////////////////////////////////////
-//  // Boolean op XXX
-//  ////////////////////////////////////////////////////////////////////////
-//  property("Boolean, Nat arguments") = {illTyped("""implicitly[OP[leftBoolean,rightNat]]"""); true}
-//  property("Boolean, Char arguments") = {illTyped("""implicitly[OP[leftBoolean,rightChar]]"""); true}
-//  property("Boolean, Int arguments") = {illTyped("""implicitly[OP[leftBoolean,rightInt]]"""); true}
-//  property("Boolean, Long arguments") = {illTyped("""implicitly[OP[leftBoolean,rightLong]]"""); true}
-//  property("Boolean, Float arguments") = {illTyped("""implicitly[OP[leftBoolean,rightFloat]]"""); true}
-//  property("Boolean, Double arguments") = {illTyped("""implicitly[OP[leftBoolean,rightDouble]]"""); true}
-//  property("Boolean, String arguments") = {illTyped("""implicitly[OP[leftBoolean,rightString]]"""); true}
-//  property("Boolean, Boolean arguments") = {illTyped("""implicitly[OP[leftBoolean,rightBoolean]]"""); true}
-//  ////////////////////////////////////////////////////////////////////////
-//}
+package singleton.ops
+
+import org.scalacheck.Properties
+import shapeless.test.illTyped
+import singleton.TestUtils._
+
+class MinusSpec extends Properties("-") {
+  type OP[L,R] = -[L,R]
+  type leftNat = shapeless.Nat._3
+  type leftChar = W.`'\u0003'`.T
+  type leftInt = W.`3`.T
+  type leftLong = W.`3L`.T
+  type leftFloat = W.`3.0f`.T
+  type leftDouble = W.`3.0`.T
+  type leftString = W.`"Something"`.T
+  type leftBoolean = True
+
+  type rightNat = shapeless.Nat._2
+  type rightChar = W.`'\u0002'`.T
+  type rightInt = W.`2`.T
+  type rightLong = W.`2L`.T
+  type rightFloat = W.`2.0f`.T
+  type rightDouble = W.`2.0`.T
+  type rightString = W.`"Else"`.T
+  type rightBoolean = False
+
+  type resultChar = W.`'\u0001'`.T
+  type resultInt = W.`1`.T
+  type resultLong = W.`1L`.T
+  type resultFloat = W.`1.0f`.T
+  type resultDouble = W.`1.0`.T
+  type resultString = W.`"SomethingElse"`.T
+  type resultBoolean = W.`false`.T
+
+  ////////////////////////////////////////////////////////////////////////
+  // Nat op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("Nat, Nat arguments") = verifyOp2Args[OP,leftNat,rightNat,resultInt]
+  property("Nat, Char arguments") = verifyOp2Args[OP,leftNat,rightChar,resultInt]
+  property("Nat, Int arguments") = verifyOp2Args[OP,leftNat,rightInt,resultInt]
+  property("Nat, Long arguments") = verifyOp2Args[OP,leftNat,rightLong,resultLong]
+  property("Nat, Float arguments") = verifyOp2Args[OP,leftNat,rightFloat,resultFloat]
+  property("Nat, Double arguments") = verifyOp2Args[OP,leftNat,rightDouble,resultDouble]
+  property("Nat, String arguments") = {illTyped("""implicitly[OP[leftNat,rightString]]"""); true}
+  property("Nat, Boolean arguments") = {illTyped("""implicitly[OP[leftNat,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Char op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("Char, Nat arguments") = verifyOp2Args[OP,leftChar,rightNat,resultInt]
+  property("Char, Char arguments") = verifyOp2Args[OP,leftChar,rightChar,resultInt]
+  property("Char, Int arguments") = verifyOp2Args[OP,leftChar,rightInt,resultInt]
+  property("Char, Long arguments") = verifyOp2Args[OP,leftChar,rightLong,resultLong]
+  property("Char, Float arguments") = verifyOp2Args[OP,leftChar,rightFloat,resultFloat]
+  property("Char, Double arguments") = verifyOp2Args[OP,leftChar,rightDouble,resultDouble]
+  property("Char, String arguments") = {illTyped("""implicitly[OP[leftChar,rightString]]"""); true}
+  property("Char, Boolean arguments") = {illTyped("""implicitly[OP[leftChar,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Int op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("Int, Nat arguments") = verifyOp2Args[OP,leftInt,rightNat,resultInt]
+  property("Int, Char arguments") = verifyOp2Args[OP,leftInt,rightChar,resultInt]
+  property("Int, Int arguments") = verifyOp2Args[OP,leftInt,rightInt,resultInt]
+  property("Int, Long arguments") = verifyOp2Args[OP,leftInt,rightLong,resultLong]
+  property("Int, Float arguments") = verifyOp2Args[OP,leftInt,rightFloat,resultFloat]
+  property("Int, Double arguments") = verifyOp2Args[OP,leftInt,rightDouble,resultDouble]
+  property("Int, String arguments") = {illTyped("""implicitly[OP[leftInt,rightString]]"""); true}
+  property("Int, Boolean arguments") = {illTyped("""implicitly[OP[leftInt,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Long op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("Long, Nat arguments") = verifyOp2Args[OP,leftLong,rightNat,resultLong]
+  property("Long, Char arguments") = verifyOp2Args[OP,leftLong,rightChar,resultLong]
+  property("Long, Int arguments") = verifyOp2Args[OP,leftLong,rightInt,resultLong]
+  property("Long, Long arguments") = verifyOp2Args[OP,leftLong,rightLong,resultLong]
+  property("Long, Float arguments") = verifyOp2Args[OP,leftLong,rightFloat,resultFloat]
+  property("Long, Double arguments") = verifyOp2Args[OP,leftLong,rightDouble,resultDouble]
+  property("Long, String arguments") = {illTyped("""implicitly[OP[leftLong,rightString]]"""); true}
+  property("Long, Boolean arguments") = {illTyped("""implicitly[OP[leftLong,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Float op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("Float, Nat arguments") = verifyOp2Args[OP,leftFloat,rightNat,resultFloat]
+  property("Float, Char arguments") = verifyOp2Args[OP,leftFloat,rightChar,resultFloat]
+  property("Float, Int arguments") = verifyOp2Args[OP,leftFloat,rightInt,resultFloat]
+  property("Float, Long arguments") = verifyOp2Args[OP,leftFloat,rightLong,resultFloat]
+  property("Float, Float arguments") = verifyOp2Args[OP,leftFloat,rightFloat,resultFloat]
+  property("Float, Double arguments") = verifyOp2Args[OP,leftFloat,rightDouble,resultDouble]
+  property("Float, String arguments") = {illTyped("""implicitly[OP[leftFloat,rightString]]"""); true}
+  property("Float, Boolean arguments") = {illTyped("""implicitly[OP[leftFloat,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Double op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("Double, Nat arguments") = verifyOp2Args[OP,leftDouble,rightNat,resultDouble]
+  property("Double, Char arguments") = verifyOp2Args[OP,leftDouble,rightChar,resultDouble]
+  property("Double, Int arguments") = verifyOp2Args[OP,leftDouble,rightInt,resultDouble]
+  property("Double, Long arguments") = verifyOp2Args[OP,leftDouble,rightLong,resultDouble]
+  property("Double, Float arguments") = verifyOp2Args[OP,leftDouble,rightFloat,resultDouble]
+  property("Double, Double arguments") = verifyOp2Args[OP,leftDouble,rightDouble,resultDouble]
+  property("Double, String arguments") = {illTyped("""implicitly[OP[leftDouble,rightString]]"""); true}
+  property("Double, Boolean arguments") = {illTyped("""implicitly[OP[leftDouble,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // String op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("String, Nat arguments") = {illTyped("""implicitly[OP[leftString,rightNat]]"""); true}
+  property("String, Char arguments") = {illTyped("""implicitly[OP[leftString,rightChar]]"""); true}
+  property("String, Int arguments") = {illTyped("""implicitly[OP[leftString,rightInt]]"""); true}
+  property("String, Long arguments") = {illTyped("""implicitly[OP[leftString,rightLong]]"""); true}
+  property("String, Float arguments") = {illTyped("""implicitly[OP[leftString,rightFloat]]"""); true}
+  property("String, Double arguments") = {illTyped("""implicitly[OP[leftString,rightDouble]]"""); true}
+  property("String, String arguments") = {illTyped("""implicitly[OP[leftString,rightString]]"""); true}
+  property("String, Boolean arguments") = {illTyped("""implicitly[OP[leftString,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////////////////////
+  // Boolean op XXX
+  ////////////////////////////////////////////////////////////////////////
+  property("Boolean, Nat arguments") = {illTyped("""implicitly[OP[leftBoolean,rightNat]]"""); true}
+  property("Boolean, Char arguments") = {illTyped("""implicitly[OP[leftBoolean,rightChar]]"""); true}
+  property("Boolean, Int arguments") = {illTyped("""implicitly[OP[leftBoolean,rightInt]]"""); true}
+  property("Boolean, Long arguments") = {illTyped("""implicitly[OP[leftBoolean,rightLong]]"""); true}
+  property("Boolean, Float arguments") = {illTyped("""implicitly[OP[leftBoolean,rightFloat]]"""); true}
+  property("Boolean, Double arguments") = {illTyped("""implicitly[OP[leftBoolean,rightDouble]]"""); true}
+  property("Boolean, String arguments") = {illTyped("""implicitly[OP[leftBoolean,rightString]]"""); true}
+  property("Boolean, Boolean arguments") = {illTyped("""implicitly[OP[leftBoolean,rightBoolean]]"""); true}
+  ////////////////////////////////////////////////////////////////////////
+}
--- src/test/scala/singleton/ops/ToConversionSpec.scala
+++ src/test/scala/singleton/ops/ToConversionSpec.scala
@@ -1,86 +1,86 @@
-//package singleton.ops
-//
-//import org.scalacheck.Properties
-//import shapeless.test.illTyped
-//import singleton.TestUtils._
-//
-//class ToConversionSpec extends Properties("ToConversion") {
-//  property("Nat to Nat") = wellTyped {
-//    def getNat(implicit op : SafeNat[ToNat[shapeless.Nat._1]]) : op.Out = op.value
-//    val ret : shapeless.nat._1 = getNat
-//  }
-//  property("Char to Nat") = wellTyped {
-//    def getNat(implicit op : SafeNat[ToNat['\u0001']]) : op.Out = op.value
-//    val ret : shapeless.nat._1 = getNat
-//  }
-//  property("Int to Nat") = wellTyped {
-//    def getNat(implicit op : SafeNat[ToNat[1]]) : op.Out = op.value
-//    val ret : shapeless.nat._1 = getNat
-//  }
-//  property("Long to Nat") = wellTyped {
-//    def getNat(implicit op : SafeNat[ToNat[1L]]) : op.Out = op.value
-//    val ret : shapeless.nat._1 = getNat
-//  }
-//  property("Float to Nat") = wellTyped {
-//    def getNat(implicit op : SafeNat[ToNat[1.0f]]) : op.Out = op.value
-//    val ret : shapeless.nat._1 = getNat
-//  }
-//  property("Double to Nat") = wellTyped {
-//    def getNat(implicit op : SafeNat[ToNat[1.0]]) : op.Out = op.value
-//    val ret : shapeless.nat._1 = getNat
-//  }
-//
-//  property("Nat to Char") = verifyOp1Args[ToChar,shapeless.Nat._1,'\u0001']
-//  property("Char to Char") = verifyOp1Args[ToChar,'\u0002','\u0002']
-//  property("Int to Char") = verifyOp1Args[ToChar,3,'\u0003']
-//  property("Long to Char") = verifyOp1Args[ToChar,4L,'\u0004']
-//  property("Float to Char") = verifyOp1Args[ToChar,5.0f,'\u0005']
-//  property("Double to Char") = verifyOp1Args[ToChar,6.0,'\u0006']
-//  property("String to Char") = {illTyped("""implicitly[ToChar["7"]]"""); true}
-//  property("Boolean to Char") = {illTyped("""implicitly[ToChar[true]]"""); true}
-//
-//  property("Nat to Int") = verifyOp1Args[ToInt,shapeless.Nat._1,1]
-//  property("Char to Int") = verifyOp1Args[ToInt,'\u0002',2]
-//  property("Int to Int") = verifyOp1Args[ToInt,3,3]
-//  property("Long to Int") = verifyOp1Args[ToInt,4L,4]
-//  property("Float to Int") = verifyOp1Args[ToInt,5.0f,5]
-//  property("Double to Int") = verifyOp1Args[ToInt,6.0,6]
-//  property("String to Int") = verifyOp1Args[ToInt,"7",7]
-//  property("Boolean to Int") = {illTyped("""implicitly[ToInt[true]]"""); true}
-//
-//  property("Nat to Long") = verifyOp1Args[ToLong,shapeless.Nat._1,1L]
-//  property("Char to Long") = verifyOp1Args[ToLong,'\u0002',2L]
-//  property("Int to Long") = verifyOp1Args[ToLong,3,3L]
-//  property("Long to Long") = verifyOp1Args[ToLong,4L,4L]
-//  property("Float to Long") = verifyOp1Args[ToLong,5.0f,5L]
-//  property("Double to Long") = verifyOp1Args[ToLong,6.0,6L]
-//  property("String to Long") = verifyOp1Args[ToLong,"7",7L]
-//  property("Boolean to Long") = {illTyped("""implicitly[ToLong[true]]"""); true}
-//
-//  property("Nat to Float") = verifyOp1Args[ToFloat,shapeless.Nat._1,1.0f]
-//  property("Char to Float") = verifyOp1Args[ToFloat,'\u0002',2.0f]
-//  property("Int to Float") = verifyOp1Args[ToFloat,3,3.0f]
-//  property("Long to Float") = verifyOp1Args[ToFloat,4L,4.0f]
-//  property("Float to Float") = verifyOp1Args[ToFloat,5.0f,5.0f]
-//  property("Double to Float") = verifyOp1Args[ToFloat,6.0,6.0f]
-//  property("String to Float") = verifyOp1Args[ToFloat,"7",7.0f]
-//  property("Boolean to Float") = {illTyped("""implicitly[ToFloat[true]]"""); true}
-//
-//  property("Nat to Double") = verifyOp1Args[ToDouble,shapeless.Nat._1,1.0]
-//  property("Char to Double") = verifyOp1Args[ToDouble,'\u0002',2.0]
-//  property("Int to Double") = verifyOp1Args[ToDouble,3,3.0]
-//  property("Long to Double") = verifyOp1Args[ToDouble,4L,4.0]
-//  property("Float to Double") = verifyOp1Args[ToDouble,5.0f,5.0]
-//  property("Double to Double") = verifyOp1Args[ToDouble,6.0,6.0]
-//  property("String to Double") = verifyOp1Args[ToDouble,"7",7.0]
-//  property("Boolean to Double") = {illTyped("""implicitly[ToDouble[true]]"""); true}
-//
-//  property("Nat to String") = verifyOp1Args[ToString,shapeless.Nat._1,"1"]
-//  property("Char to String") = verifyOp1Args[ToString,'2',"2"]
-//  property("Int to String") = verifyOp1Args[ToString,3,"3"]
-//  property("Long to String") = verifyOp1Args[ToString,4L,"4"]
-//  property("Float to String") = verifyOp1Args[ToString,5.0f,"5.0"]
-//  property("Double to String") = verifyOp1Args[ToString,6.0,"6.0"]
-//  property("String to String") = verifyOp1Args[ToString,"7","7"]
-//  property("Boolean to String") = verifyOp1Args[ToString,true,"true"]
-//}
+package singleton.ops
+
+import org.scalacheck.Properties
+import shapeless.test.illTyped
+import singleton.TestUtils._
+
+class ToConversionSpec extends Properties("ToConversion") {
+  property("Nat to Nat") = wellTyped {
+    def getNat(implicit op : SafeNat[ToNat[shapeless.Nat._1]]) : op.Out = op.value
+    val ret : shapeless.nat._1 = getNat
+  }
+  property("Char to Nat") = wellTyped {
+    def getNat(implicit op : SafeNat[ToNat[W.`'\u0001'`.T]]) : op.Out = op.value
+    val ret : shapeless.nat._1 = getNat
+  }
+  property("Int to Nat") = wellTyped {
+    def getNat(implicit op : SafeNat[ToNat[W.`1`.T]]) : op.Out = op.value
+    val ret : shapeless.nat._1 = getNat
+  }
+  property("Long to Nat") = wellTyped {
+    def getNat(implicit op : SafeNat[ToNat[W.`1L`.T]]) : op.Out = op.value
+    val ret : shapeless.nat._1 = getNat
+  }
+  property("Float to Nat") = wellTyped {
+    def getNat(implicit op : SafeNat[ToNat[W.`1.0f`.T]]) : op.Out = op.value
+    val ret : shapeless.nat._1 = getNat
+  }
+  property("Double to Nat") = wellTyped {
+    def getNat(implicit op : SafeNat[ToNat[W.`1.0`.T]]) : op.Out = op.value
+    val ret : shapeless.nat._1 = getNat
+  }
+
+  property("Nat to Char") = verifyOp1Args[ToChar,shapeless.Nat._1,W.`'\u0001'`.T]
+  property("Char to Char") = verifyOp1Args[ToChar,W.`'\u0002'`.T,W.`'\u0002'`.T]
+  property("Int to Char") = verifyOp1Args[ToChar,W.`3`.T,W.`'\u0003'`.T]
+  property("Long to Char") = verifyOp1Args[ToChar,W.`4L`.T,W.`'\u0004'`.T]
+  property("Float to Char") = verifyOp1Args[ToChar,W.`5.0f`.T,W.`'\u0005'`.T]
+  property("Double to Char") = verifyOp1Args[ToChar,W.`6.0`.T,W.`'\u0006'`.T]
+  property("String to Char") = {illTyped("""implicitly[ToChar[W.`"7"`.T]]"""); true}
+  property("Boolean to Char") = {illTyped("""implicitly[ToChar[True]]"""); true}
+
+  property("Nat to Int") = verifyOp1Args[ToInt,shapeless.Nat._1,W.`1`.T]
+  property("Char to Int") = verifyOp1Args[ToInt,W.`'\u0002'`.T,W.`2`.T]
+  property("Int to Int") = verifyOp1Args[ToInt,W.`3`.T,W.`3`.T]
+  property("Long to Int") = verifyOp1Args[ToInt,W.`4L`.T,W.`4`.T]
+  property("Float to Int") = verifyOp1Args[ToInt,W.`5.0f`.T,W.`5`.T]
+  property("Double to Int") = verifyOp1Args[ToInt,W.`6.0`.T,W.`6`.T]
+  property("String to Int") = verifyOp1Args[ToInt,W.`"7"`.T,W.`7`.T]
+  property("Boolean to Int") = {illTyped("""implicitly[ToInt[True]]"""); true}
+
+  property("Nat to Long") = verifyOp1Args[ToLong,shapeless.Nat._1,W.`1L`.T]
+  property("Char to Long") = verifyOp1Args[ToLong,W.`'\u0002'`.T,W.`2L`.T]
+  property("Int to Long") = verifyOp1Args[ToLong,W.`3`.T,W.`3L`.T]
+  property("Long to Long") = verifyOp1Args[ToLong,W.`4L`.T,W.`4L`.T]
+  property("Float to Long") = verifyOp1Args[ToLong,W.`5.0f`.T,W.`5L`.T]
+  property("Double to Long") = verifyOp1Args[ToLong,W.`6.0`.T,W.`6L`.T]
+  property("String to Long") = verifyOp1Args[ToLong,W.`"7"`.T,W.`7L`.T]
+  property("Boolean to Long") = {illTyped("""implicitly[ToLong[True]]"""); true}
+
+  property("Nat to Float") = verifyOp1Args[ToFloat,shapeless.Nat._1,W.`1.0f`.T]
+  property("Char to Float") = verifyOp1Args[ToFloat,W.`'\u0002'`.T,W.`2.0f`.T]
+  property("Int to Float") = verifyOp1Args[ToFloat,W.`3`.T,W.`3.0f`.T]
+  property("Long to Float") = verifyOp1Args[ToFloat,W.`4L`.T,W.`4.0f`.T]
+  property("Float to Float") = verifyOp1Args[ToFloat,W.`5.0f`.T,W.`5.0f`.T]
+  property("Double to Float") = verifyOp1Args[ToFloat,W.`6.0`.T,W.`6.0f`.T]
+  property("String to Float") = verifyOp1Args[ToFloat,W.`"7"`.T,W.`7.0f`.T]
+  property("Boolean to Float") = {illTyped("""implicitly[ToFloat[True]]"""); true}
+
+  property("Nat to Double") = verifyOp1Args[ToDouble,shapeless.Nat._1,W.`1.0`.T]
+  property("Char to Double") = verifyOp1Args[ToDouble,W.`'\u0002'`.T,W.`2.0`.T]
+  property("Int to Double") = verifyOp1Args[ToDouble,W.`3`.T,W.`3.0`.T]
+  property("Long to Double") = verifyOp1Args[ToDouble,W.`4L`.T,W.`4.0`.T]
+  property("Float to Double") = verifyOp1Args[ToDouble,W.`5.0f`.T,W.`5.0`.T]
+  property("Double to Double") = verifyOp1Args[ToDouble,W.`6.0`.T,W.`6.0`.T]
+  property("String to Double") = verifyOp1Args[ToDouble,W.`"7"`.T,W.`7.0`.T]
+  property("Boolean to Double") = {illTyped("""implicitly[ToDouble[True]]"""); true}
+
+  property("Nat to String") = verifyOp1Args[ToString,shapeless.Nat._1,W.`"1"`.T]
+  property("Char to String") = verifyOp1Args[ToString,W.`'2'`.T,W.`"2"`.T]
+  property("Int to String") = verifyOp1Args[ToString,W.`3`.T,W.`"3"`.T]
+  property("Long to String") = verifyOp1Args[ToString,W.`4L`.T,W.`"4"`.T]
+  property("Float to String") = verifyOp1Args[ToString,W.`5.0f`.T,W.`"5.0"`.T]
+  property("Double to String") = verifyOp1Args[ToString,W.`6.0`.T,W.`"6.0"`.T]
+  property("String to String") = verifyOp1Args[ToString,W.`"7"`.T,W.`"7"`.T]
+  property("Boolean to String") = verifyOp1Args[ToString,True,W.`"true"`.T]
+}
