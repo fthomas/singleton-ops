0ce16797affa52ffc98cafed5c66dd4176abe86c
Oron Port <soronpo@campus.technion.ac.il>
2017-08-02 10:41:45 +0300

    slowly converting tests

--- src/test/scala/singleton/ops/LengthSpec.scala
+++ src/test/scala/singleton/ops/LengthSpec.scala
@@ -1,11 +1,11 @@
-//package singleton.ops
-//
-//import org.scalacheck.Properties
-//import singleton.TestUtils._
-//
-//class LengthSpec extends Properties("Length") {
-//  property("foobar.length == 6") = wellTyped {
-//    def length[P1 <: XString](implicit op : Length[P1]) : op.Out{} = op.value
-//    val r : 6 = length["foobar"]
-//  }
-//}
+package singleton.ops
+
+import org.scalacheck.Properties
+import singleton.TestUtils._
+
+class LengthSpec extends Properties("Length") {
+  property("foobar.length == 6") = wellTyped {
+    def length[P1 <: XString](implicit op : Length[P1]) : op.Out{} = op.value
+    val r : W.`6`.T = length[W.`"foobar"`.T]
+  }
+}
