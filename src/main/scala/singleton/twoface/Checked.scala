4569db5cf4350361b10ea12eede4326631e3f44c
Oron Port <soronpo@campus.technion.ac.il>
2017-12-09 04:17:02 +0200

    Added template comments

--- src/main/scala/singleton/twoface/Checked.scala
+++ src/main/scala/singleton/twoface/Checked.scala
@@ -51,36 +51,141 @@ object Checked0Param {
 object Checked1Param {
   //CC: traits used for the checked class
   //CO: traits used for the checked companion object
+
+  ///////////////////////////////////////////////
+  // Template to define a Checked.Char type
+  ///////////////////////////////////////////////
+  //object /* Name this object */ {
+  //  type Cond[T, P] = /* Your condition here */
+  //  type Msg[T, P] = /* Your message here */
+  //  type ParamFace = /* The parameter base type (e.g., Int) */
+  //  final class Checked[T, Param](val value : Char) extends AnyVal with Checked1Param.Char.CC[Checked, Cond, Msg, T, ParamFace, Param] {
+  //    @inline def getValue : Char = value
+  //  }
+  //  object Checked extends Checked1Param.Char.CO[Checked, Cond, Msg, ParamFace]
+  //  object WorkAround extends impl.Checked1ParamAny.Builder[Nothing, Nothing, Nothing, Nothing, Nothing]
+  //}
+  ///////////////////////////////////////////////
   object Char {
     trait CC[Chk[_,_], Cond[_,_], Msg[_,_], T, ParamFace, Param] extends Any with
       Checked1ParamAny[Chk, Cond, Msg, std.Char, T, ParamFace, Param] with TwoFaceAny.Char[T]
     trait CO[Chk[_,_], Cond[_,_], Msg[_,_], ParamFace] extends Checked1ParamAny.Builder[Chk, Cond, Msg, std.Char, ParamFace]
   }
+
+  ///////////////////////////////////////////////
+  // Template to define a Checked.Int type
+  ///////////////////////////////////////////////
+  //object /* Name this object */ {
+  //  type Cond[T, P] = /* Your condition here */
+  //  type Msg[T, P] = /* Your message here */
+  //  type ParamFace = /* The parameter base type (e.g., Int) */
+  //  final class Checked[T, Param](val value : Int) extends AnyVal with Checked1Param.Int.CC[Checked, Cond, Msg, T, ParamFace, Param] {
+  //    @inline def getValue : Int = value
+  //  }
+  //  object Checked extends Checked1Param.Int.CO[Checked, Cond, Msg, ParamFace]
+  //  object WorkAround extends impl.Checked1ParamAny.Builder[Nothing, Nothing, Nothing, Nothing, Nothing]
+  //}
+  ///////////////////////////////////////////////
   object Int {
     trait CC[Chk[_,_], Cond[_,_], Msg[_,_], T, ParamFace, Param] extends Any with
       Checked1ParamAny[Chk, Cond, Msg, std.Int, T, ParamFace, Param] with TwoFaceAny.Int[T]
     trait CO[Chk[_,_], Cond[_,_], Msg[_,_], ParamFace] extends Checked1ParamAny.Builder[Chk, Cond, Msg, std.Int, ParamFace]
   }
+
+  ///////////////////////////////////////////////
+  // Template to define a Checked.Long type
+  ///////////////////////////////////////////////
+  //object /* Name this object */ {
+  //  type Cond[T, P] = /* Your condition here */
+  //  type Msg[T, P] = /* Your message here */
+  //  type ParamFace = /* The parameter base type (e.g., Int) */
+  //  final class Checked[T, Param](val value : Long) extends AnyVal with Checked1Param.Long.CC[Checked, Cond, Msg, T, ParamFace, Param] {
+  //    @inline def getValue : Long = value
+  //  }
+  //  object Checked extends Checked1Param.Long.CO[Checked, Cond, Msg, ParamFace]
+  //  object WorkAround extends impl.Checked1ParamAny.Builder[Nothing, Nothing, Nothing, Nothing, Nothing]
+  //}
+  ///////////////////////////////////////////////
   object Long {
     trait CC[Chk[_,_], Cond[_,_], Msg[_,_], T, ParamFace, Param] extends Any with
       Checked1ParamAny[Chk, Cond, Msg, std.Long, T, ParamFace, Param] with TwoFaceAny.Long[T]
     trait CO[Chk[_,_], Cond[_,_], Msg[_,_], ParamFace] extends Checked1ParamAny.Builder[Chk, Cond, Msg, std.Long, ParamFace]
   }
+
+  ///////////////////////////////////////////////
+  // Template to define a Checked.Float type
+  ///////////////////////////////////////////////
+  //object /* Name this object */ {
+  //  type Cond[T, P] = /* Your condition here */
+  //  type Msg[T, P] = /* Your message here */
+  //  type ParamFace = /* The parameter base type (e.g., Int) */
+  //  final class Checked[T, Param](val value : Float) extends AnyVal with Checked1Param.Float.CC[Checked, Cond, Msg, T, ParamFace, Param] {
+  //    @inline def getValue : Float = value
+  //  }
+  //  object Checked extends Checked1Param.Float.CO[Checked, Cond, Msg, ParamFace]
+  //  object WorkAround extends impl.Checked1ParamAny.Builder[Nothing, Nothing, Nothing, Nothing, Nothing]
+  //}
+  ///////////////////////////////////////////////
   object Float {
     trait CC[Chk[_,_], Cond[_,_], Msg[_,_], T, ParamFace, Param] extends Any with
       Checked1ParamAny[Chk, Cond, Msg, std.Float, T, ParamFace, Param] with TwoFaceAny.Float[T]
     trait CO[Chk[_,_], Cond[_,_], Msg[_,_], ParamFace] extends Checked1ParamAny.Builder[Chk, Cond, Msg, std.Float, ParamFace]
   }
+
+  ///////////////////////////////////////////////
+  // Template to define a Checked.Double type
+  ///////////////////////////////////////////////
+  //object /* Name this object */ {
+  //  type Cond[T, P] = /* Your condition here */
+  //  type Msg[T, P] = /* Your message here */
+  //  type ParamFace = /* The parameter base type (e.g., Int) */
+  //  final class Checked[T, Param](val value : Double) extends AnyVal with Checked1Param.Double.CC[Checked, Cond, Msg, T, ParamFace, Param] {
+  //    @inline def getValue : Double = value
+  //  }
+  //  object Checked extends Checked1Param.Double.CO[Checked, Cond, Msg, ParamFace]
+  //  object WorkAround extends impl.Checked1ParamAny.Builder[Nothing, Nothing, Nothing, Nothing, Nothing]
+  //}
+  ///////////////////////////////////////////////
   object Double {
     trait CC[Chk[_,_], Cond[_,_], Msg[_,_], T, ParamFace, Param] extends Any with
       Checked1ParamAny[Chk, Cond, Msg, std.Double, T, ParamFace, Param] with TwoFaceAny.Double[T]
     trait CO[Chk[_,_], Cond[_,_], Msg[_,_], ParamFace] extends Checked1ParamAny.Builder[Chk, Cond, Msg, std.Double, ParamFace]
   }
+
+  ///////////////////////////////////////////////
+  // Template to define a Checked.String type
+  ///////////////////////////////////////////////
+  //object /* Name this object */ {
+  //  type Cond[T, P] = /* Your condition here */
+  //  type Msg[T, P] = /* Your message here */
+  //  type ParamFace = /* The parameter base type (e.g., Int) */
+  //  final class Checked[T, Param](val value : String) extends AnyVal with Checked1Param.String.CC[Checked, Cond, Msg, T, ParamFace, Param] {
+  //    @inline def getValue : String = value
+  //  }
+  //  object Checked extends Checked1Param.String.CO[Checked, Cond, Msg, ParamFace]
+  //  object WorkAround extends impl.Checked1ParamAny.Builder[Nothing, Nothing, Nothing, Nothing, Nothing]
+  //}
+  ///////////////////////////////////////////////
   object String {
     trait CC[Chk[_,_], Cond[_,_], Msg[_,_], T, ParamFace, Param] extends Any with
       Checked1ParamAny[Chk, Cond, Msg, std.String, T, ParamFace, Param] with TwoFaceAny.String[T]
     trait CO[Chk[_,_], Cond[_,_], Msg[_,_], ParamFace] extends Checked1ParamAny.Builder[Chk, Cond, Msg, std.String, ParamFace]
   }
+
+  ///////////////////////////////////////////////
+  // Template to define a Checked.Boolean type
+  ///////////////////////////////////////////////
+  //object /* Name this object */ {
+  //  type Cond[T, P] = /* Your condition here */
+  //  type Msg[T, P] = /* Your message here */
+  //  type ParamFace = /* The parameter base type (e.g., Int) */
+  //  final class Checked[T, Param](val value : Boolean) extends AnyVal with Checked1Param.Boolean.CC[Checked, Cond, Msg, T, ParamFace, Param] {
+  //    @inline def getValue : Boolean = value
+  //  }
+  //  object Checked extends Checked1Param.Boolean.CO[Checked, Cond, Msg, ParamFace]
+  //  object WorkAround extends impl.Checked1ParamAny.Builder[Nothing, Nothing, Nothing, Nothing, Nothing]
+  //}
+  ///////////////////////////////////////////////
   object Boolean {
     trait CC[Chk[_,_], Cond[_,_], Msg[_,_], T, ParamFace, Param] extends Any with
       Checked1ParamAny[Chk, Cond, Msg, std.Boolean, T, ParamFace, Param] with TwoFaceAny.Boolean[T]
