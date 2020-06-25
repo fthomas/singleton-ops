14abaa44fc0a9a24002b46957c8aa7efdfca7f04
Erik Erlandson <eerlands@redhat.com>
2020-06-24 17:42:23 -0700

    remove type bounds for GCD

--- src/main/scala/singleton/ops/rational.scala
+++ src/main/scala/singleton/ops/rational.scala
@@ -17,6 +17,42 @@ object rational {
     def show(implicit nv: Id[N], dv: Id[D]): String = s"Rational(${n}, ${d})"
   }
 
+  trait AlignTypeOpId
+  type AlignType[V, P] = OpMacro[AlignTypeOpId, V, P, W.`0`.T]
+  implicit def doAlignTypeInt[V, P <: XInt](implicit
+      v: ToInt[V]): OpIntercept.Aux[AlignType[V, P], v.Out] = ???
+  implicit def doAlignTypeLong[V, P <: XLong](implicit
+      v: ToLong[V]): OpIntercept.Aux[AlignType[V, P], v.Out] = ???
+  implicit def doAlignTypeFloat[V, P <: XFloat](implicit
+      v: ToFloat[V]): OpIntercept.Aux[AlignType[V, P], v.Out] = ???
+  implicit def doAlignTypeDouble[V, P <: XDouble](implicit
+      v: ToDouble[V]): OpIntercept.Aux[AlignType[V, P], v.Out] = ???
+
+  trait IsPositiveOpId
+  type IsPositive[P] = OpMacro[IsPositiveOpId, P, W.`0`.T, W.`0`.T]
+  implicit def doIsPositive[P](implicit
+      tst: P > AlignType[0, P]): OpIntercept.Aux[IsPositive[P], tst.Out] = ???
+
+  trait IsNegativeOpId
+  type IsNegative[P] = OpMacro[IsNegativeOpId, P, W.`0`.T, W.`0`.T]
+  implicit def doIsNegative[P](implicit
+      tst: P < AlignType[0, P]): OpIntercept.Aux[IsNegative[P], tst.Out] = ???
+
+  trait IsZeroOpId
+  type IsZero[P] = OpMacro[IsZeroOpId, P, W.`0`.T, W.`0`.T]
+  implicit def doIsZero[P](implicit
+      tst: P == AlignType[0, P]): OpIntercept.Aux[IsZero[P], tst.Out] = ???
+
+  trait IsNonZeroOpId
+  type IsNonZero[P] = OpMacro[IsNonZeroOpId, P, W.`0`.T, W.`0`.T]
+  implicit def doIsNonZero[P](implicit
+      tst: P != AlignType[0, P]): OpIntercept.Aux[IsNonZero[P], tst.Out] = ???
+
+  trait IsIntegralOpId
+  type IsIntegral[P] = OpMacro[IsIntegralOpId, P, W.`0`.T, W.`0`.T]
+  implicit def doIsIntegral[P](implicit
+      tst: IsInt[P] || IsLong[P]): OpIntercept.Aux[IsIntegral[P], tst.Out] = ???
+
   private trait IsRationalImpl[P] {
     type Out
   }
@@ -47,21 +83,22 @@ object rational {
       N <: XInt, D <: XInt,
       SN <: XInt, SD <: XInt](
     implicit
-      sim: OpGen.Aux[Simplify[Rational[N, D]], Rational[SN, SD]]
+      sim: OpAuxGen[Simplify[Rational[N, D]], Rational[SN, SD]]
     ): OpIntercept.Aux[ToRational[Rational[N, D]], Rational[SN, SD]] =
     new OpIntercept[ToRational[Rational[N, D]]] {
       type Out = Rational[SN, SD]
       val value: Out = new Rational[SN, SD] {}
     }
 
-  implicit def toRationalFromInt[N <: XInt]: OpIntercept.Aux[ToRational[N], Rational[N, W.`1`.T]] =
+  implicit def toRationalFromInt[N, One](implicit
+      one: OpAuxGen[AlignType[1, N], One]): OpIntercept.Aux[ToRational[N], Rational[N, One]] =
     new OpIntercept[ToRational[N]] {
-      type Out = Rational[N, W.`1`.T]
-      val value: Out = new Rational[N, W.`1`.T] {}
+      type Out = Rational[N, One]
+      val value: Out = new Rational[N, One] {}
     }
 
-  implicit def doRationalNegate[N <: XInt, D <: XInt, NN <: XInt](implicit
-      neg: OpInt.Aux[Negate[N], NN]): OpIntercept.Aux[Negate[Rational[N, D]], Rational[NN, D]] =
+  implicit def doRationalNegate[N, D, NN](implicit
+      neg: OpAuxGen[Negate[N], NN]): OpIntercept.Aux[Negate[Rational[N, D]], Rational[NN, D]] =
     new OpIntercept[Negate[Rational[N, D]]] {
       type Out = Rational[NN, D]
       val value: Out = new Rational[NN, D] {}
@@ -76,13 +113,13 @@ object rational {
       SN <: XInt, SD <: XInt](
     implicit
       rat: Require[IsRational[LHS] || IsRational[RHS]],
-      lhs: OpGen.Aux[ToRational[LHS], Rational[LN, LD]],
-      rhs: OpGen.Aux[ToRational[RHS], Rational[RN, RD]],
+      lhs: OpAuxGen[ToRational[LHS], Rational[LN, LD]],
+      rhs: OpAuxGen[ToRational[RHS], Rational[RN, RD]],
       ev0: OpInt.Aux[LN * RD, LNRD],
       ev1: OpInt.Aux[RN * LD, RNLD],
       ev2: OpInt.Aux[LNRD + RNLD, N],
       ev3: OpInt.Aux[LD * RD, D],
-      ev4: OpGen.Aux[Simplify[Rational[N, D]], Rational[SN, SD]],
+      ev4: OpAuxGen[Simplify[Rational[N, D]], Rational[SN, SD]],
     ): OpIntercept.Aux[LHS + RHS, Rational[SN, SD]] =
   new OpIntercept[LHS + RHS] {
     type Out = Rational[SN, SD]
@@ -96,10 +133,10 @@ object rational {
       SN <: XInt, SD <: XInt](
     implicit
       rat: Require[IsRational[LHS] || IsRational[RHS]],
-      lhs: OpGen.Aux[ToRational[LHS], Rational[LN, LD]],
-      rhs: OpGen.Aux[ToRational[RHS], Rational[RN, RD]],
+      lhs: OpAuxGen[ToRational[LHS], Rational[LN, LD]],
+      rhs: OpAuxGen[ToRational[RHS], Rational[RN, RD]],
       neg: OpInt.Aux[Negate[RN], RNN],
-      add: OpGen.Aux[Rational[LN, LD] + Rational[RNN, RD], Rational[SN, SD]]
+      add: OpAuxGen[Rational[LN, LD] + Rational[RNN, RD], Rational[SN, SD]]
     ): OpIntercept.Aux[LHS - RHS, Rational[SN, SD]] =
   new OpIntercept[LHS - RHS] {
     type Out = Rational[SN, SD]
@@ -114,11 +151,11 @@ object rational {
       SN <: XInt, SD <: XInt](
     implicit
       rat: Require[IsRational[LHS] || IsRational[RHS]],
-      lhs: OpGen.Aux[ToRational[LHS], Rational[LN, LD]],
-      rhs: OpGen.Aux[ToRational[RHS], Rational[RN, RD]],
+      lhs: OpAuxGen[ToRational[LHS], Rational[LN, LD]],
+      rhs: OpAuxGen[ToRational[RHS], Rational[RN, RD]],
       ev0: OpInt.Aux[LN * RN, N],
       ev1: OpInt.Aux[LD * RD, D],
-      ev2: OpGen.Aux[Simplify[Rational[N, D]], Rational[SN, SD]]
+      ev2: OpAuxGen[Simplify[Rational[N, D]], Rational[SN, SD]]
     ): OpIntercept.Aux[LHS * RHS, Rational[SN, SD]] =
     new OpIntercept[LHS * RHS] {
       type Out = Rational[SN, SD]
@@ -132,9 +169,9 @@ object rational {
       SN <: XInt, SD <: XInt](
     implicit
       rat: Require[IsRational[LHS] || IsRational[RHS]],
-      lhs: OpGen.Aux[ToRational[LHS], Rational[LN, LD]],
-      rhs: OpGen.Aux[ToRational[RHS], Rational[RN, RD]],
-      mul: OpGen.Aux[Rational[LN, LD] * Rational[RD, RN], Rational[SN, SD]]
+      lhs: OpAuxGen[ToRational[LHS], Rational[LN, LD]],
+      rhs: OpAuxGen[ToRational[RHS], Rational[RN, RD]],
+      mul: OpAuxGen[Rational[LN, LD] * Rational[RD, RN], Rational[SN, SD]]
     ): OpIntercept.Aux[LHS / RHS, Rational[SN, SD]] =
     new OpIntercept[LHS / RHS] {
       type Out = Rational[SN, SD]
@@ -144,22 +181,22 @@ object rational {
   trait GCDOpId
   type GCD[A, B] = OpMacro[GCDOpId, A, B, W.`0`.T]
 
-  private type gcdErrorMsg = W.`"GCD requires positive integers"`.T
+  private type gcdErrorMsg = W.`"GCD requires positive integral arguments"`.T
 
-  implicit def doGCDforBasisCase[A <: XInt, B <: XInt, Rem <: XInt](implicit
-      ev0: RequireMsg[(A >= B) && (B > W.`0`.T), gcdErrorMsg],
-      ev1: OpInt.Aux[A % B, Rem],
-      ev2: Require[Rem == W.`0`.T]): OpIntercept.Aux[GCD[A, B], B] = ???
+  implicit def doGCDforBasisCase[A, B, Rem](implicit
+      ev0: RequireMsg[IsIntegral[A] && IsIntegral[B] && (A >= B) && IsPositive[B], gcdErrorMsg],
+      ev1: OpAuxGen[A % B, Rem],
+      ev2: Require[IsZero[Rem]]): OpIntercept.Aux[GCD[A, B], B] = ???
 
-  implicit def doGCDforAgeB[A <: XInt, B <: XInt,Rem <: XInt, D <: XInt](implicit
-      ev0: RequireMsg[(A >= B) && (B > W.`0`.T), gcdErrorMsg],
-      ev1: OpInt.Aux[A % B, Rem],
-      ev2: Require[Rem != W.`0`.T],
-      ev3: OpInt.Aux[GCD[B, Rem], D]): OpIntercept.Aux[GCD[A, B], D] = ???
+  implicit def doGCDforAgeB[A, B, Rem, D](implicit
+      ev0: RequireMsg[IsIntegral[A] && IsIntegral[B] && (A >= B) && IsPositive[B], gcdErrorMsg],
+      ev1: OpAuxGen[A % B, Rem],
+      ev2: Require[IsNonZero[Rem]],
+      ev3: OpAuxGen[GCD[B, Rem], D]): OpIntercept.Aux[GCD[A, B], D] = ???
 
-  implicit def doGCDforAltB[A <: XInt, B <: XInt, Rem <: XInt, D <: XInt](implicit
-      ev0: RequireMsg[(A < B) && (A > W.`0`.T), gcdErrorMsg],
-      ev1: OpInt.Aux[GCD[B, A], D]): OpIntercept.Aux[GCD[A, B], D] = ???
+  implicit def doGCDforAltB[A, B, Rem, D](implicit
+      ev0: RequireMsg[IsIntegral[A] && IsIntegral[B] && (A < B) && IsPositive[A], gcdErrorMsg],
+      ev1: OpAuxGen[GCD[B, A], D]): OpIntercept.Aux[GCD[A, B], D] = ???
 
   trait SimplifyOpId
   type Simplify[F] = OpMacro[SimplifyOpId, F, W.`0`.T, W.`0`.T]
@@ -188,9 +225,9 @@ object rational {
       SN <: XInt,  SD <: XInt](
     implicit
       ev0: RequireMsg[(N < W.`0`.T) && (D > W.`0`.T), simplifyErrorMsg],
-      ev1: OpGen.Aux[Negate[Rational[N, D]], F],
-      ev2: OpGen.Aux[Simplify[F], SNF],
-      ev3: OpGen.Aux[Negate[SNF], Rational[SN, SD]]
+      ev1: OpAuxGen[Negate[Rational[N, D]], F],
+      ev2: OpAuxGen[Simplify[F], SNF],
+      ev3: OpAuxGen[Negate[SNF], Rational[SN, SD]]
     ): OpIntercept.Aux[Simplify[Rational[N, D]], Rational[SN, SD]] =
     new OpIntercept[Simplify[Rational[N, D]]] {
       type Out = Rational[SN, SD]
@@ -213,7 +250,7 @@ object rational {
       bn: RequireMsg[D < W.`0`.T, simplifyErrorMsg],
       nn: OpInt.Aux[Negate[N], NN],
       nd: OpInt.Aux[Negate[D], ND],
-      sf: OpGen.Aux[Simplify[Rational[NN, ND]], Rational[SN, SD]]
+      sf: OpAuxGen[Simplify[Rational[NN, ND]], Rational[SN, SD]]
     ): OpIntercept.Aux[Simplify[Rational[N, D]], Rational[SN, SD]] =
     new OpIntercept[Simplify[Rational[N, D]]] {
       type Out = Rational[SN, SD]
