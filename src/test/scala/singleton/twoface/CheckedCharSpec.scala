babba57f06e826867e5bb4584c41160f015ca33d
Peng Cheng <pc175@uow.edu.au>
2022-03-19 17:49:45 -0400

    upgrade all scala versions
    add an error case to RequireSpec to demonstrate the problem

--- src/test/scala/singleton/twoface/CheckedCharSpec.scala
+++ src/test/scala/singleton/twoface/CheckedCharSpec.scala
@@ -20,8 +20,8 @@ class CheckedCharSpec extends Properties("Checked.Char") {
   property("Compile-time checks") = wellTyped {
     smallerThan50('\u0020')
     smallerThan50(TwoFace.Char('\u0020'))
-    illTyped("""smallerThan50('\u0032')""")
-    illTyped("""smallerThan50(TwoFace.Char('\u0032'))""")
+    illTyped("smallerThan50('\\u0032')")
+    illTyped("smallerThan50(TwoFace.Char('\u0032'))")
   }
 
   property("Run-time checks") = wellTyped {
